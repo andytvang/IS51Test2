08ff2432a841a3d22765ad5424c6c66cc06443d8
andytvang <andytvang@mail.fresnostate.edu>
2021-04-29 18:57:50 -0700

    finish structured-english

---
+++ test_2.py
@@ -0,0 +1,15 @@
+
+
+"""
+start
+
+create a list to show all 24 numbers (float)
+capture user's input 24 times for their grades.
+each time we capture the user's inout, we append the number to the list
+sort the list ascending, then splice the items starting at index 2
+once we have the percentage of grades that are above the average grade in the list,
+we sum them up and divide by the amount numbers of percentage of grades that are above the average grade
+output a message to the user
+
+end
+"""
